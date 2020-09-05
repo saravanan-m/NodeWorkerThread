e4beedae81505b282f23a4019386fdd73abb0f30
saravanan-m <srvn.my@gmail.com>
2020-09-05 21:21:58 +0530

    pre-post process

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+const request = require('request');
+const util = require('util');
+
+const promiseRequest = util.promisify(request);
 
 const { Worker } = require('worker_threads');
 const { StaticPool } = require("node-worker-threads-pool");
@@ -10,8 +14,86 @@ const pool = new StaticPool({
   workerData: "workerData!",
 });
 
+const postData = {
+  "currencyCode": "USD",
+  "originDestinations": [
+    {
+      "id": "1",
+      "originLocationCode": "RIO",
+      "destinationLocationCode": "MAD",
+      "departureDateTimeRange": {
+        "date": "2021-02-01",
+        "time": "10:00:00"
+      }
+    },
+    {
+      "id": "2",
+      "originLocationCode": "MAD",
+      "destinationLocationCode": "RIO",
+      "departureDateTimeRange": {
+        "date": "2021-02-05",
+        "time": "17:00:00"
+      }
+    }
+  ],
+  "travelers": [
+    {
+      "id": "1",
+      "travelerType": "ADULT"
+    },
+    {
+      "id": "2",
+      "travelerType": "CHILD"
+    }
+  ],
+  "sources": [
+    "GDS"
+  ],
+  "searchCriteria": {
+    "maxFlightOffers": 2,
+    "flightFilters": {
+      "cabinRestrictions": [
+        {
+          "cabin": "BUSINESS",
+          "coverage": "MOST_SEGMENTS",
+          "originDestinationIds": [
+            "1"
+          ]
+        }
+      ],
+      "carrierRestrictions": {
+        "excludedCarrierCodes": [
+          "AA",
+          "TP",
+          "AZ"
+        ]
+      }
+    }
+  }
+};
+
 app.get('/', function (req, res) {
-  res.send('Worker thread!');
+  res.send('Healthy');
+});
+
+app.get('/pre-post', function (req, res) {
+  (async () => {
+    const preProcessResponse = await pool.exec({type:'pre-process',input:'input'});
+    var dataResponse = '';
+    try{
+      dataResponse = await promiseRequest({
+      method: 'post',
+      headers: {'content-type' : 'application/json','X-HTTP-Method-Override':'GET','Authorization':'Bearer hscAyKGQpCGWHKaH08ptWzutkkA7'},
+      url:     'https://test.api.amadeus.com/v2/shopping/flight-offers',
+      json:     postData
+    });
+    }catch(err){
+      console.log(err);
+    }
+    const postProcessResposne = await pool.exec({type:'post-process',input:'input'});
+   
+    res.send(dataResponse);
+  })();
 });
 
 app.get('/createWorker/:delaySecond', function (req, res) {
--- worker.js
+++ worker.js
@@ -1,18 +1,37 @@
 const {parentPort, workerData} = require('worker_threads');
-
+const delay_second = 3;
 
 parentPort.on("message", (param) => {
-    console.log('input param '+param.delay);
+  console.log('input param '+JSON.stringify(param));
     
-    var startTime = new Date().getTime();
-    while((new Date().getTime() - startTime) < (param.delay*1000)){
+   var resposneData = '';
+   switch(param.type){
+     case 'pre-process':
+       resposneData = amadeusPreProcess(param.input);
+       break;
+     case 'post-process':
+       resposneData = amadeusPostProcess(param.input);
+       break;  
+   }
 
-    }
-    var waitSec = (new Date().getTime() - startTime)/1000;
-  
-    console.log('wait sec '+waitSec);
-    // return the result to main thread.
-    parentPort.postMessage({wait:waitSec});
+    parentPort.postMessage({response:resposneData});
   });
 
+  function amadeusPreProcess(input){
+    var startTime = new Date().getTime();
+    while((new Date().getTime() - startTime) < (delay_second*1000)){
+           //cpu heavy process
+    }
+    console.log('pre process completed');
+    return 'pre-process-response';
+  }
+
+  function amadeusPostProcess(input){
+    var startTime = new Date().getTime();
+    while((new Date().getTime() - startTime) < (delay_second*1000)){
+           //cpu heavy process
+    }
+    console.log('post process completed');
+    return 'post-process-response';
+  }
 
